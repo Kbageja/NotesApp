83ae8eb3738b6812d421315761504c305616e3dc
kinshuk bageja <kinshukbageja@gmail.com>
2025-09-30 00:42:24 +0530

    updatednodemailer node

--- backend/src/config/emails.ts
+++ backend/src/config/emails.ts
@@ -6,7 +6,7 @@ const createTransporter = () => {
     service: 'gmail', // Use Gmail service
     host: process.env.EMAIL_HOST!,
     port: parseInt(process.env.EMAIL_PORT!) || 587,
-    secure: false, // true for 465, false for other ports like 587
+    secure: true, // true for 465, false for other ports like 587
     auth: {
       user: process.env.EMAIL_USER!,
       pass: process.env.EMAIL_PASS!,
